e91e7fb36f97281e1f761937850aa9d7db745b06
Enzo <enzoledentu@gmail.com>
2021-12-01 17:05:38 +0100

    fix path bug

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.graph import Graph
 from src.node import Node
 from tools.algorithms import *
+from models.hanoi import *
 
 
 if __name__ == '__main__':
@@ -44,3 +45,19 @@ if __name__ == '__main__':
 
     D_access_hash = graph.hash_depth_first_search(D)
     print(f'Accessibles nodes from {D} : {D_access_hash}')
+
+    print("\n-- Hanoi Model")
+    hanoi = Hanoi(3, 3)
+    initial = hanoi.initial()
+    print(f'Graph initial: {initial}')
+    step1 = hanoi.next(initial)
+    print(f'\nStep_1   : {step1}')
+    for i in range(len(step1)):
+        print(f'Step_2.{i} : {hanoi.next(step1[i])}')
+
+    print('\n-- Hash --')
+    for i in range(len(step1)):
+        print(f'Step_{i}   : {hash(step1[i])} -- {step1[i]}')
+        step2 = hanoi.next(step1[i])
+        for j in range(len(step2)):
+            print(f'Step_{i}.{j} : {hash(step2[j])} -- {step2[j]}')

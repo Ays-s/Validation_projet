9754418935a87be9e2f08fd83b11acfd2d06096c
Hugo <hugo.questroy@gmail.com>
2021-12-08 15:00:04 +0100

    change behaviour

--- src/kernel.py
+++ src/kernel.py
@@ -52,18 +52,3 @@ class BehaviourSoupSemantics(SemanticTransitionRelation):
         target = copy.deepcopy(c)
         r = a(target)
         return target
-
-class BehaviorSoup:
-    def __init__(self, conf):
-        self.inital = conf
-        self.behavior = []
-
-    def add(self, name, guard, action):
-        self.behavior.append(Behavior(name, guard, action))
-
-
-class Behavior:
-    def __init__(self, name, guard, action):
-        self.name = name
-        self.guard = guard
-        self.action = action
